a1ba074e543869ec00b1372f905c029ca89d8be9
feigi <Feigi7063@gmail.com>
2024-07-30 14:54:58 +0300

    add apiUrl to prod

--- src/enviroments/environment.prod.ts
+++ src/enviroments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
-    // apiUrl: 'https://your-production-api.com/api',
+    apiUrl: 'https://miri-rizel-server.onrender.com/',
+    CLIENT_ID : "592088999303-njnsom6mvdcn0kpesi2ibtlhmblcfcda.apps.googleusercontent.com",
+    API_KEY : "AIzaSyAe4wCi-LL97X_H7fhfaYnB_0cPqfdYVNU"
   };
   
