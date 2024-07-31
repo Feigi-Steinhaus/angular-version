f827b9989905db53cf476f89d2690ee741b2b3a4
Feigi-S <feigi7063@gmail.com>
2024-07-31 10:59:32 +0300

    update enviroment

--- src/enviroments/environment.prod.ts
+++ src/enviroments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://miri-rizel-server.onrender.com/',
+    apiUrl: 'https://miri-rizel-server-nw23.onrender.com/',
     CLIENT_ID : "592088999303-njnsom6mvdcn0kpesi2ibtlhmblcfcda.apps.googleusercontent.com",
     API_KEY : "AIzaSyAe4wCi-LL97X_H7fhfaYnB_0cPqfdYVNU"
   };
